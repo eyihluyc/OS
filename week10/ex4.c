061be0b87df461edf98a8cc056572e4d410a1130
franisulla <theviolthevioletandthewine@gmail.com>
2020-10-20 21:26:26 +0300

    almost ex4

---
+++ week10/ex4.c
@@ -0,0 +1,54 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <stddef.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	DIR* dir = opendir("temp");
+	int n = 0;
+	struct dirent *dp;
+	while ((dp = readdir(dir)) != NULL){
+		n++;
+	}
+	(void) closedir(dir);
+	
+
+	int* inodes = calloc(n, sizeof(int));
+	char** names = calloc(n*n, 256);
+	int* counter = calloc(n, sizeof(int));
+
+	int next_available = 0;
+	dir = opendir("temp");
+	while ((dp = readdir(dir)) != NULL){
+		int inode = dp -> d_ino;
+		int i=0;
+		for (i; i<n; i++){
+			if (inodes[i] == inode){
+				break;
+			}
+		}
+		if (i == n){
+			i = next_available;
+			next_available++;
+			inodes[i] = inode;
+		}
+		names[i*n+counter[i]] = dp -> d_name;
+		counter[i]++;
+		
+	}
+	(void) closedir(dir);
+
+	FILE *fp = fopen("ex4.txt", "w");	
+	for (int i=0; i<n; i++){
+		if (counter[i] > 1){
+			for (int j=0; j<counter[i]; j++){
+				printf("%s ", names[i*n+j]);
+			}
+			printf("\n");
+		}
+
+	}
+	return 0;
+}
